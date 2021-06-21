3cc598fc6e4243beb0445f9b9d47213930ff4d9e
alo <mail@aloispaulus.be>
2021-06-21 16:20:51 +0200

    Update to last release of swift

--- zwiftj-cdn/src/main/java/zwiftj/cdn/controller/CDNController.java
+++ zwiftj-cdn/src/main/java/zwiftj/cdn/controller/CDNController.java
@@ -5,29 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 @Controller
 public class CDNController {
 
     @GetMapping(value = "/gameassets/Zwift_Updates_Root/", produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
     public String gameAssets() {
-        return "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n" +
-                "<title>Directory listing for /gameassets/Zwift_Updates_Root/</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1>Directory listing for /gameassets/Zwift_Updates_Root/</h1>\n" +
-                "<hr>\n" +
-                "<ul>\n" +
-                "<li><a href=\"Launcher_ver_cur.xml\">Launcher_ver_cur.xml</a></li>\n" +
-                "<li><a href=\"LauncherMac_ver_cur.xml\">LauncherMac_ver_cur.xml</a></li>\n" +
-                "<li><a href=\"Zwift_ver_cur.xml\">Zwift_ver_cur.xml</a></li>\n" +
-                "<li><a href=\"ZwiftMac_ver_cur.xml\">ZwiftMac_ver_cur.xml</a></li>\n" +
-                "</ul>\n" +
-                "<hr>\n" +
-                "</body>\n" +
-                "</html>";
+        InputStream fileStream = this.getClass().getResourceAsStream("/templates/Zwift_Updates_Root_listing.html");
+        try {
+            return new String(fileStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
